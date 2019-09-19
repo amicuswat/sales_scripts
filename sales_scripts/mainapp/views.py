5daa7d8962899c0ea547d02ed548705fb35fb09c
pythonwizard <amicuswat@gmail.com>
2019-09-19 10:44:12 +0400

    Created main page with place holders for menu and content. Yet to be filled.

--- sales_scripts/mainapp/views.py
+++ sales_scripts/mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    title = 'главная'
+
+    content = {
+        'title': title
+    }
+
+    return render(request, 'mainapp/index.html', content)
--- sales_scripts/sales_scripts/urls.py
+++ sales_scripts/sales_scripts/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('', include('mainapp.urls', namespace='main')),
+    path('', mainapp.main, name='main'),
 ]
