b97f1022ba06b832c9a7415760529447aa45d8d1
pythonwizard <amicuswat@gmail.com>
2019-09-22 11:34:35 +0400

    Start of shop creation

--- sales_scripts/marketapp/urls.py
+++ sales_scripts/marketapp/urls.py
@@ -1,7 +1,9 @@
-
+from django.urls import path
+import marketapp.views as marketapp
 
 app_name = 'marketapp'
 
 urlpatterns = [
-    
+    path('sd_shop/', marketapp.sd_shop, name='sd_shop'),
+    path('buy/', marketapp.buy_sd, name='buy_sd'),
 ]
--- sales_scripts/marketapp/views.py
+++ sales_scripts/marketapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def buy_sd(request, money, quantity):
+    title = 'Магазин с/д'
+
+    content ={
+        'title': title
+    }
+    pass
+
+def sd_shop(request):
+    title = 'Магазин с/д'
+
+    content = {
+        'title': title
+    }
+
+    return render(request, 'marketapp/sd_shop.html', content)
