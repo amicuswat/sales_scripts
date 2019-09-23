2cacbe386926e2293cc86213b5d009dd004d6fb4
pythonwizard <amicuswat@gmail.com>
2019-09-23 11:13:50 +0400

    designed the SD shop page

--- sales_scripts/marketapp/views.py
+++ sales_scripts/marketapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.urls import reverse
 from authapp.models import ScriptsUser
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/auth/login/')
 def buy_sd(request, amount, quantity):
     title = 'Магазин с/д'
 
@@ -16,13 +18,66 @@ def buy_sd(request, amount, quantity):
     }
     return HttpResponseRedirect(reverse('admin:control_post'))
 
+@login_required(login_url='/auth/login/')
 def sd_shop(request):
     title = 'Магазин с/д'
     container_size = 'medium_container'
+    prices = [
+        {
+            'volume': 20,
+            'price': 12,
+            'economy': 0,
+            'to_pay': 240,
+            'message': 'Кэп, ну как то так, далеко не улетим, да и дорого, чисто попробовать.'
+        },
+        {
+            'volume': 50,
+            'price': 11,
+            'economy': 50,
+            'to_pay': 550,
+            'message': 'Кэп, ну это уже куда не шло, месячишку полетаем, да и на чупачупс сэкономим.'
+        },
+        {
+            'volume': 100,
+            'price': 10,
+            'economy': 200,
+            'to_pay': 1000,
+            'message': 'Кэп, можно брать, на несколько полетов хватит, и еще детям на конфетки останется.'
+        },
+        {
+            'volume': 250,
+            'price': 9,
+            'economy': 750,
+            'to_pay': 2250,
+            'message': 'Кэп, отличный выбор, и не дорого и нормально полетать можно.'
+        },
+        {
+            'volume': 500,
+            'price': 8,
+            'economy': 2000,
+            'to_pay': 4000,
+            'message': 'Кэп, то что надо. Грузить? Нам на долго хватит и еще на пол столько же останется.'
+        },
+        {
+            'volume': 1000,
+            'price': 7,
+            'economy': 5000,
+            'to_pay': 7000,
+            'message': 'Кэп, да мы с такими запасами... Эээххх... И не дорого совсе.'
+        },
+        {
+            'volume': 5000,
+            'price': 6,
+            'economy': 30000,
+            'to_pay': 30000,
+            'message': 'Кэп, нам этого на всю жизнь хватит и внукам останется, надо брать. Да и бесплатно почти отдают.'
+        },
+    ]
 
     content = {
         'title': title,
-        'container_size': container_size
+        'container_size': container_size,
+        'prices': prices
     }
 
     return render(request, 'marketapp/sd_shop.html', content)
